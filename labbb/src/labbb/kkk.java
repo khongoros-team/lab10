ea4acc925118e04c9bb9f1ace086bdb622008d81
Admin <Admin@DESKTOP-9VS7AV9>
2024-04-03 10:39:09 +0800

    enkhtsetseg

--- labbb/src/labbb/kkk.java
+++ labbb/src/labbb/kkk.java
@@ -4,7 +4,7 @@ public class kkk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Enkhtsetseg bainaa");
+System.out.println("Our team");
 	}
 
 }
