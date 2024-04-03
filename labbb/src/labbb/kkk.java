879b1b1a5e7305179fea573c22e07d662ed58346
Admin <Admin@DESKTOP-9VS7AV9>
2024-04-03 10:28:51 +0800

    sec commit

--- labbb/src/labbb/kkk.java
+++ labbb/src/labbb/kkk.java
@@ -4,7 +4,7 @@ public class kkk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome to my team!");
+System.out.println("Welcome to my team");
 	}
 
 }
